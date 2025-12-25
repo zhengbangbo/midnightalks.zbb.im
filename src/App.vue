f89d240b3ab732ce3080ad9294b1e1767f60bfef
𝘉𝘰𝘣 𝘡𝘩𝘦𝘯𝘨 <zbbgoforit@gmail.com>
2025-12-25 22:35:09 +0800

    feat: update page title and enhance footer with source code links

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@ import { useHead } from '@vueuse/head'
 import BaseLayout from './layout/BaseLayout.vue'
 
 const headDefault = {
-  title: '大内密谈',
+  title: '大内密谈封面浏览器',
   meta: [
     { name: 'description', content: '快速浏览《大内密谈》的往期精美封面' },
     { name: 'keywords', content: 'Podcast, Chinese, Music' },
--- src/components/DeclarationBanner.vue
+++ src/components/DeclarationBanner.vue
@@ -5,7 +5,7 @@
 <template>
   <footer relative text-center py-6 leading-7>
     <p>
-      本网站非官方网站，由<a bg-gradient-to-r from-rose-400 via-fuchsia-500 to-indigo-500 bg-clip-text text-transparent href="https://github.com/bobz25">听众</a>开发。
+      本网站非官方网站，由<a bg-gradient-to-r from-rose-400 via-fuchsia-500 to-indigo-500 bg-clip-text text-transparent href="https://github.com/zhengbangbo" target="_blank" rel="noopener noreferrer">听众</a>开发，源代码见<a bg-gradient-to-r from-rose-400 via-fuchsia-500 to-indigo-500 bg-clip-text text-transparent href="https://github.com/zhengbangbo/midnightalks.zbb.im" target="_blank" rel="noopener noreferrer">GitHub</a>。
     </p>
   </footer>
 </template>
