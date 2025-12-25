fd6da5b03d6c0c0d1830080a2ae923a16a261ced
𝘉𝘰𝘣 𝘡𝘩𝘦𝘯𝘨 <zbbgoforit@gmail.com>
2025-12-25 22:39:57 +0800

    fix: update URL in mock handler for correct API endpoint

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -2,5 +2,5 @@ import { http, HttpResponse } from 'msw'
 import { mockXml } from './resolvers/mockXml'
 
 export const handlers = [
-  http.get('https://cors-anywhere.dev.imzbb.cc/https%3A%2F%2Frss.lizhi.fm%2Frss%2F14275.xml', mockXml),
+  http.get('https://cors-anywhere.zbb.workers.dev/https%3A%2F%2Frss.lizhi.fm%2Frss%2F14275.xml', mockXml),
 ]
